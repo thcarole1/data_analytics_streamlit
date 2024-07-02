57955587f1c89a38070fb935bfb4112df00ec982
Thierry CAROLE <thcarole1@hotmail.com>
2024-07-02 04:51:21 +0200

    Minimum working product

--- app/app.py
+++ app/app.py
@@ -4,54 +4,6 @@ import pandas as pd
 import json
 from io import StringIO
 
-# if 'page' not in st.session_state:
-#         st.session_state.page = 'Home'
-
-# # Define pages
-# def home():
-#     st.title("Home Page")
-#     st.write("Welcome to the Home Page")
-
-#     if st.button('Go to Page 1'):
-#                 st.session_state.page = 'Page 1'
-#     if st.button('Go to Page 2'):
-#                 st.session_state.page = 'Page 2'
-#     if st.button('Go to Page 3'):
-#                 st.session_state.page = 'Page 3'
-
-# def page1():
-#     st.title("Page 1")
-#     st.write("Welcome to Page 1")
-#     if st.button('Go to Home'):
-#                 st.session_state.page = 'Home'
-
-# def page2():
-#     st.title("Page 2")
-#     st.write("Welcome to Page 2")
-#     if st.button('Go to Home'):
-#                 st.session_state.page = 'Home'
-
-# def page3():
-#     st.title("Page 3")
-#     st.write("Welcome to Page 3")
-#     if st.button('Go to Home'):
-#                 st.session_state.page = 'Home'
-
-# # Display the selected page
-# if st.session_state.page == 'Home':
-#     home()
-# elif st.session_state.page == 'Page 1':
-#     page1()
-# elif st.session_state.page == 'Page 2':
-#     page2()
-# elif st.session_state.page == 'Page 3':
-#     page3()
-
-
-# url = 'https://file-us-3.sendbird.com/4458ae52de1e4146b1a6c08e2939d7bd.png?auth=aHC8odu2s_CoY9f2dIQQ2u8Oofk9YAHKmOY-tq7AGuh8E52La3iUaYOJCFf3Fm8ml5lVfdDX1KhI8hR_9L6W7A'
-# st.image(url)
-
-
 st.markdown(
     """ # HR Data analytics
     ## Analysing HR data to predict and improve employee attrition rates.
